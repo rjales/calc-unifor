873c283281a7c7fdabce3f00a9ef0e73f11b8b83
Romulo Jales <romulojales@outlook.com>
2022-11-16 00:35:14 -0300

    correção

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function calcularMedia() {
     let av1 = parseFloat(document.getElementById("av1").value);
     let av2 = parseFloat(document.getElementById("av2").value);
     let av3 = parseFloat(document.getElementById("av3").value);
-    
+
     if (av1 >= 0 && av1 <= 10 && av2 >= 0 && av2 <= 10 && av3 >= 0 && av3 <= 10) {
         var notaFinal = ((((av1 + av2) / 2) + av3) / 2);
     } else {
-        alert("Somentes notas entre 0 e 10 são aceitas.");
+        alert("Somente notas entre 0 e 10 são aceitas.");
     }
-    
+
     if ((((av1 + av2) / 2)) < 4) {
         document.getElementById('resultado').innerHTML = "Reprovado, pois a média entre AV1 e AV2 foi menor que 4,0.";
     } else if (av3 < 4) {
